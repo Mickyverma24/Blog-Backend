954466fa0274d96107f7b07f7a1769c2bb3f56b4
Ravi verma <vermaravi8809@gmail.com>
2024-03-12 01:01:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const PORT =  process.env.PORT || 9002;
+
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/users");
+const postRoutes = require("./routes/posts");
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDb connected successfully"))
+  .catch((err) => console.log(err.message));
+
+app.get("/", (req, res) => {
+  res.send("If you are reading means our backend is working.. Hurray");
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
